780c1a60e6559d39782b8aaac9d009d8615296bf
Frank W. Bergmann <csstidy-c@tuxad.com>
2015-05-27 23:42:40 +0200

    update the internal version strings to the current version number

--- csstidy/csspp_private.h
+++ csstidy/csspp_private.h
@@ -5,19 +5,19 @@
 #define CSSPP_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.3.0.2232"
+#define VER_STRING	"1.4.0.0"
 #define VER_MAJOR	1
 #define VER_MINOR	3
 #define VER_RELEASE	0
 #define VER_BUILD	2232
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.3"
+#define FILE_VERSION	"1.4"
 #define FILE_DESCRIPTION	"CSS Parser and Optimiser"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"CSSTidy"
-#define PRODUCT_VERSION	"1.3"
+#define PRODUCT_VERSION	"1.4"
 
 #endif /*CSSPP_PRIVATE_H*/
